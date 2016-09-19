751a19042ecf3b6240fcdf5de6bb8e07aa49ed65
kayke <kayke.ducati@outlook.com>
2016-09-18 23:24:14 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,41 +2,35 @@
 
 function sendText() {
     var htpp = document.getElementById("htpp").value;
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', htpp, true);
-    xhr.onload = function (d) {
-        if (this.status == 200) {
-            document.getElementById("resposta").innerHTML = this.responseText;
+    var luo = new XMLHttpRequest();
+    var resposta = document.getElementById("lua");
+    luo.open('GET', htpp, true);
+    luo.onload = function (e) {
+        if (this.status === 200) {
+       
             var body = this.responseText;
             var jsonData = JSON.parse(body);
             var result = jsonData.result;
-            var res = result.length;
-            if (res > 0) {
+            if (result.length > 0) {
                 msg = "";
-                for (i = res - 1; i > 0; i--) {
+                for (i = result.length - 1; i >= 0; i--) {
                     var text = result[i].message.text;
                     var nome = result[i].message.from.first_name;
                     var hora = result[i].message.date;
-                    var time = hora;
-                    var d = new Date(time * 1000);
-                    var n = d.toLocaleDateString();
-                    var g = d.toLocaleTimeString();
+                    var d = new Date(hora * 1000);
+                    var data = d.toLocaleDateString();
+                    var horaF = d.toLocaleTimeString();
 
-                    document.title = res + " " + " @MestreDosMagoss_bot"; 
-                    msg = "<p>" + msg + (n + " </br>" + g + " " + " " + nome + ":" + "</br> " + text) + "</p>";
+                    document.title = result.length + " " + " @MestreDosMagoss_bot"; 
+                    //msg = "<p>" + msg + "<span class=\"parag\">" + (data + " - " + horaF + " " + " " + nome + ": </span>" + "</br><span class=\"texto\"> " + text) + "</span></p>";
+                    msg = "<p>" + msg + "<span class=\"parag\">" + data + " - " + horaF + " " + nome + ": </span>" + "</p>" + "<p>" + "<span class=\"texto\">" + text + "</span>" + "</p>"; 
                     resposta.innerHTML = msg;
+                    
                 }
             }
         }
     };
-    xhr.send();
+    luo.send();
 }
 
-//setInterval(sendText, 1000);
-
-function limpar() {//
-    var limpar = document.getElementById("resposta");
-    limpar.innerHTML = "";
-
-}
+setInterval(sendText, 1000);
